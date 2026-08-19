33bd0cacdaf38aaa68200219912665a4c30167a1
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2026-08-19 17:07:05 +0530

    updated to display customer details in cards

--- src/app/customer-component/customer-component.ts
+++ src/app/customer-component/customer-component.ts
@@ -12,12 +12,15 @@ import { CommonModule } from '@angular/common';
 export class CustomerComponent {
 
   private customerService = inject(CustomerService);
+  customerServiceResponse: any; // Variable to hold the response from the service
   custId: number = 0; // Example customer ID, you can change this as needed
   customerName: string = ''; // Example customer name, you can change this as needed
   onShowAllCustomers(): void {
     console.log('Show all customers button clicked');
     this.customerService.getAllCustomers().subscribe((customers) => {
       console.log('Retrieved customers:', customers);
+      //this.customerServiceResponse = JSON.stringify(customers, null, 2); // Store the response as a string
+      this.customerServiceResponse = customers; // Store the response as an object to iterate over in the template
     });
   }
 
@@ -26,6 +29,8 @@ export class CustomerComponent {
     // Implement logic to show details for the specified customer
     this.customerService.getCustomerById(this.custId).subscribe((customer) => {
       console.log('Retrieved customer details:', customer);
+     // this.customerServiceResponse = JSON.stringify(customer, null, 2); // Store the response as a string
+     this.customerServiceResponse = customer; // Store the response as an object to iterate over in the template
     });
   }
 
@@ -33,6 +38,8 @@ export class CustomerComponent {
     console.log('Show customer details by name button clicked');  
     this.customerService.getCustomerByName(this.customerName).subscribe((customer) => {
       console.log('Retrieved customer details by name:', customer);
+     // this.customerServiceResponse = JSON.stringify(customer, null, 2); // Store the response as a string
+     this.customerServiceResponse = customer; // Store the response as an object to iterate over in the template
     });
   }
 
